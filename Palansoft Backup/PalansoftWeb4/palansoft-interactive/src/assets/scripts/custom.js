8b0e94328f0e0448a7f982da3e9b1098df52a89f
Gretwork <gretwork@gmail.com>
2022-04-01 12:13:14 +0530

    Palansoft Interactive Website Demo 1
    Palansoft Interactive Website Demo 1

--- Palansoft Backup/PalansoftWeb4/palansoft-interactive/src/assets/scripts/custom.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', result)
-
-function result() {
-  let URL = window.location.pathname
-  let page = URL.split('/').pop().split('.').shift()
-  document.querySelector('body').classList.add(`page${page}`)
-}
--- Palansoft Backup/PalansoftWeb7/palansoft-interactive/palansoft-interactive/src/api/palansoftApi.jsx
+++
@@ -1,6 +0,0 @@
-import axios from 'axios'
-const instance = axios.create({
-  baseURL:
-    'http://www.hereinoman.com:3000/api/v1/palansoftInteractive/activity/activityListing',
-})
-export default instance
